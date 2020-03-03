20878314d2b63b34102074413c2e71d7944343e4
Jenny Klich <jennyklich@gmail.com>
2020-03-03 15:47:09 -0700

    Add discount update test

--- spec/features/merchant/discounts/update_spec.rb
+++ spec/features/merchant/discounts/update_spec.rb
@@ -1,63 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Discount Update Page" do
-  it "can update an existing discount" do
-    merchant_1 = Merchant.create(name:'Megans Creatures', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-    m_user = merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, role: 1, email: 'merchant@example.com', password: 'password')
-    discount_1 = merchant_1.discounts.create(name: "Ten Item Discount", percentage_off: 20, minimum_quantity: 10)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(m_user)
+  describe "as a merchant" do
+    it "can update an existing discount" do
+      merchant_1 = Merchant.create(name:'Megans Creatures', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      m_user = merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, role: 1, email: 'merchant@example.com', password: 'password')
+      discount_1 = merchant_1.discounts.create(name: "Ten Item Discount", percentage_off: 20, minimum_quantity: 10)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(m_user)
 
-    visit "/merchant/discounts"
+      visit "/merchant/discounts"
 
-    click_link "Edit Discount"
+      click_link "Edit Discount"
 
-    expect(current_path).to eq("/merchant/discounts/#{discount_1.id}/edit")
+      expect(current_path).to eq("/merchant/discounts/#{discount_1.id}/edit")
 
-    fill_in :name, with: "new discount"
-    fill_in :percentage_off, with: 10
-    fill_in :minimum_quantity, with: 10
+      fill_in :name, with: "new discount"
+      fill_in :percentage_off, with: 10
+      fill_in :minimum_quantity, with: 10
 
-    click_on "Update Discount"
+      click_on "Update Discount"
 
-    expect(current_path).to eq("/merchant/discounts")
+      expect(current_path).to eq("/merchant/discounts")
 
-    within "#discount#{discount_1.id}" do
-      expect(page).to have_content("new discount")
-      expect(page).to have_content("Percentage: 10")
-      expect(page).to have_content("Minimum Quantity: 10")
+      within "#discount#{discount_1.id}" do
+        expect(page).to have_content("new discount")
+        expect(page).to have_content("Percentage: 10")
+        expect(page).to have_content("Minimum Quantity: 10")
+      end
     end
   end
 end
-
-# require 'rails_helper'
-#
-# RSpec.describe "Discount Update Page" do
-#   it "can update an existing discount" do
-#     User.destroy_all
-#     Merchant.destroy_all
-#     merchant_1 = Merchant.create(name: 'Megans Creatures', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-#     m_user = merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, role: 1, email: 'merchant@example.com', password: 'password')
-#     discount_1 = merchant_1.discounts.create(name: "Ten Item Discount", percentage_off: 20, minimum_quantity: 10)
-#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(m_user)
-#
-#     visit "/merchant/discounts"
-#
-#     click_link "Edit Discount"
-#
-#     expect(current_path).to eq("/merchant/discounts/#{discount_1.id}/edit")
-#
-#     fill_in :name, with: "new discount"
-#     fill_in :percentage_off, with: 5
-#     fill_in :minimum_quantity, with: 5
-#
-#     click_on "Update Discount"
-#
-#     expect(current_path).to eq("/merchant/discounts")
-#
-#     within "#discount#{discount_1.id}" do
-#       expect(discount_1.name).to have_content("new discount")
-#       expect(page).to have_content("Percentage: 5")
-#       expect(page).to have_content("Minimum Quantity: 5")
-#     end
-#   end
-# end
